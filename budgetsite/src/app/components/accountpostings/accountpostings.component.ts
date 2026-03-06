d9edbc5b30dfe38ee93eba09b9c2cad2dd00d29b
Johnny Frits <frits.johnny@gmail.com>
2026-03-06 15:48:12 -0300

    fix: comentar coluna de ações na tabela de lançamentos

--- budgetsite/src/app/components/accountpostings/accountpostings.component.ts
+++ budgetsite/src/app/components/accountpostings/accountpostings.component.ts
@@ -67,7 +67,7 @@ export class AccountPostingsComponent implements OnInit, AfterViewInit {
     'description',
     'amount',
     'runningAmount',
-    'actions',
+    // 'actions',
   ];
   accountApplicationsDisplayedColumns = [
     'indexApplication',
